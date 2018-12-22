a3025cac3b018adf192417eeba0240ce7478a9f7
Jeppe Bundsgaard <jeppe@bundsgaard.net>
2018-12-22 13:28:11 +0100

    Delete tempory files

--- templates/main.php
+++ templates/main.php
@@ -6,6 +6,10 @@ if(!$_SESSION["userid"]) exit;
 
 #List of available tasks, number of responses in each, click->go to scoring of task
 <a href="#" class="scoreTask" data-task-id="0">Score task 0</a>
+
+
+#Training: List of available tasks, number of responses in each, click->go to training of task
+
 #My flags
 
 #My stats
